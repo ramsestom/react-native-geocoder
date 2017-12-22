8db93d925516ad929ba53ca5e1eef02f4fdc9f50
ramsestom <ramsestom@github.com>
2017-12-22 03:35:08 +0100

    Remove @override on createJSModules for latest RN version

--- android/src/main/java/com/devfd/RNGeocoder/RNGeocoderPackage.java
+++ android/src/main/java/com/devfd/RNGeocoder/RNGeocoderPackage.java
@@ -20,7 +20,7 @@ public class RNGeocoderPackage implements ReactPackage {
     );
   }
 
-  @Override
+  
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
